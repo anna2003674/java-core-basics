db09aaec3ad441b1dbaece80605afb01ce842d4d
Анна Теремизова <amura780@gmail.com>
2025-07-04 23:09:14 +0300

    constructors

--- src/main/java/org/basic/java/string/Human.java
+++ src/main/java/org/basic/java/string/Human.java
@@ -2,7 +2,7 @@ package org.basic.java.string;
 
 class Lesson19 {
     public static void main(String[] args) {
-        Human human1 = new Human("Kolya", 35);
+        Human human1 = new Human("Kolya", 55);
         human1.getInfo();
     }
 }
@@ -11,7 +11,22 @@ public class Human {
     private String name;
     private int age;
 
+    public Human() {
+        System.out.println("Привет из первого конструктора");
+    }
+
+    public Human(String name) {
+        System.out.println("Привет из второго конструктора");
+        this.name = name;
+    }
+
+    public Human(int age) {
+        System.out.println("Привет из третьего конструктора");
+        this.age = age;
+    }
+
     public Human(String name, int age) {
+        System.out.println("Привет из четвертого конструктора");
         this.name = name;
         this.age = age;
     }
